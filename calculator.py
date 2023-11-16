0e6d80f3ada26965a3337390f435af5747cd8490
Marcin Snoch <marcin.msx@gmail.com>
2023-11-16 20:18:04 +0100

    +

--- calculator.py
+++ calculator.py
@@ -11,9 +11,30 @@ class Calculator(QMainWindow):
         self.initUi()
 
     def initUi(self):
-        self.lbl_number1 = QtWidgets.QLabel(self)
-        self.lbl_number1.setText("Number1")
-        self.lbl_number1.move(50, 30)
+        self.lbl_name = QtWidgets.QLabel(self)
+        self.lbl_name.setText("Enter your name: ")
+        self.lbl_name.move(50, 50)
+
+        self.lbl_surname = QtWidgets.QLabel(self)
+        self.lbl_surname.setText("Enter your surname: ")
+        self.lbl_surname.move(50, 90)
+
+        self.txt_name = QtWidgets.QLineEdit(self)
+        self.txt_name.move(200, 50)
+        self.txt_name.resize(200, 32)
+
+        self.txt_surname = QtWidgets.QLineEdit(self)
+        self.txt_surname.move(200, 90)
+        self.txt_surname.resize(200, 32)
+
+        self.btn_save = QtWidgets.QPushButton(self)
+        self.btn_save.setText("Save")
+        self.btn_save.move(200, 130)
+
+        self.lbl_result = QtWidgets.QLabel(self)
+        self.lbl_result.setText('RESULT')
+        self.lbl_result.move(200, 170)
+        self.lbl_result.resize(200, 150)
 
 
 def app():
